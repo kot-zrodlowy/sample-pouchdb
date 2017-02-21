ca1ac52194aab5fa572e5e3fb4e58c0687b6d034
Kornelia <korneliak.95@gmail.com>
2017-02-21 22:38:04 +0100

    Add JS Doc comment to DB API

--- js/db.js
+++ js/db.js
@@ -6,6 +6,10 @@ module.exports = (() => {
     };
     const toEatBase = new PouchDB(CONFIG.localName);
     return {
+        /**
+         * @function addItem - adding item to DB
+         * @param item {Object} - item to add to DB
+         */
         addItem: (item) => {
             toEatBase.put(item).then((res) => {
                 console.log("Item added");
@@ -14,6 +18,10 @@ module.exports = (() => {
                 console.error("Cannot add item" + err);
             })
         },
+        /**
+         * @function removeItem - remove item gave as param from DB
+         * @param item {Object} - Item from remove
+         */
         removeItem: (item) => {
             toEatBase.remove(item).then((res) => {
                 console.log("Item removed");
@@ -22,6 +30,10 @@ module.exports = (() => {
                 console.error("Cannot remove this item " + err);
             });
         },
+        /**
+         * @function editItem - edit item gave in parameter
+         * @param item {Object} - item to edit
+         */
         editItem: (item) => {
             toEatBase.get(item._id).then((res) => {
                 toEatBase.put(item).then((es) => {
@@ -33,6 +45,10 @@ module.exports = (() => {
                 console.error("Cannot getting item" + err);
             })
         },
+        /**
+         * @function list all items from DB
+         * @param callback {Function} - action which you want to do with data. It should have res object as a param.
+         */
         listItems: (callback) => {
             toEatBase.allDocs({
                 include_docs: true,
